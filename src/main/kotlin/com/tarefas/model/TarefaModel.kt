e0b26a4609c38bd4761b4dc30b1f006b41e5cb7b
lucascarneiroandrade <lucascarneiroandrade@gmail.com>
2025-12-05 11:39:11 -0300

    Retirados imports não utilizados do TarefaModel

--- src/main/kotlin/com/tarefas/model/TarefaModel.kt
+++ src/main/kotlin/com/tarefas/model/TarefaModel.kt
@@ -1,13 +1,7 @@
 package com.tarefas.model
 
 import com.tarefas.enums.TarefaStatusEnum
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity(name = "tarefa")
